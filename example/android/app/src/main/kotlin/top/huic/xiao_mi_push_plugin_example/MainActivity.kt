c4cc1fc329cb465db18a409e84630f37ce7ecb19
jiangjuhong <jjh@huic.top>
2020-05-24 18:48:58 +0800

    初始化项目

---
+++ example/android/app/src/main/kotlin/top/huic/xiao_mi_push_plugin_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package top.huic.xiao_mi_push_plugin_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Classes/XiaoMiPushPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface XiaoMiPushPlugin : NSObject<FlutterPlugin>
+@end
